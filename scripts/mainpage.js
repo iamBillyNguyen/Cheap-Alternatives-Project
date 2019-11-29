84f51082b641b16716d218ab632df978032eb992
Billy Nguyen <bquynhnguyen209@gmail.com>
2019-11-29 13:44:16 -0800

    changed database ID

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
     })
 })
 
-db.collection("Bathroom").doc("dt.0001").onSnapshot(function (snap) {
+db.collection("Bathroom").doc("b1").onSnapshot(function (snap) {
     console.log("Current data is ... ", snap.data().name);
-    document.getElementById("name1").innerHTML = snap.data().name; //Get name value from document
+    document.getElementById("name1").innerHTML = snap.data()
+        .name; //Get name value from document
     document.getElementById("rating1").innerHTML = snap.data()
         .rating; //Get rating value from document
     document.getElementById("location1").innerHTML = snap.data()
@@ -18,10 +19,10 @@ db.collection("Bathroom").doc("dt.0001").onSnapshot(function (snap) {
         .Like; //Get like
     console.log("Current likes: ", snap.data().Like);
 
-    setAddListener("dt.0001");
+    setAddListener("1");
 });
 
-db.collection("Bathroom").doc("bcit.0001").onSnapshot(function (snap) {
+db.collection("Bathroom").doc("b2").onSnapshot(function (snap) {
     console.log("Current data is ... ", snap.data().name);
     document.getElementById("name2").innerHTML = snap.data().name; //Get name value from document
     document.getElementById("rating2").innerHTML = snap.data()
@@ -33,40 +34,10 @@ db.collection("Bathroom").doc("bcit.0001").onSnapshot(function (snap) {
         .Like; //Get like
     console.log("Current likes: ", snap.data().Like);
 
-    setAddListener("bcit.0001");
+    setAddListener("2");
 });
 
-db.collection("Bathroom").doc("dt.0002").onSnapshot(function (snap) {
-    console.log("Current data is ... ", snap.data().name);
-    document.getElementById("name4").innerHTML = snap.data().name; //Get name value from document
-    document.getElementById("rating4").innerHTML = snap.data()
-        .rating; //Get rating value from document
-    document.getElementById("location4").innerHTML = snap.data()
-        .location; //Get location value from document
-
-    document.getElementById("likes4").innerHTML = snap.data()
-        .Like; //Get like
-    console.log("Current likes: ", snap.data().Like);
-
-    setAddListener("dt.0002");
-});
-
-db.collection("Bathroom").doc("bcit.0001").onSnapshot(function (snap) {
-    console.log("Current data is ... ", snap.data().name);
-    document.getElementById("name2").innerHTML = snap.data().name; //Get name value from document
-    document.getElementById("rating2").innerHTML = snap.data()
-        .rating; //Get rating value from document
-    document.getElementById("location2").innerHTML = snap.data()
-        .location; //Get location value from document
-
-    document.getElementById("likes2").innerHTML = snap.data()
-        .Like; //Get like
-    console.log("Current likes: ", snap.data().Like);
-
-    setAddListener("bcit.0001");
-});
-
-db.collection("Bathroom").doc("bcit.0002").onSnapshot(function (snap) {
+db.collection("Bathroom").doc("b3").onSnapshot(function (snap) {
     console.log("Current data is ... ", snap.data().name);
     document.getElementById("name3").innerHTML = snap.data().name; //Get name value from document
     document.getElementById("rating3").innerHTML = snap.data()
@@ -78,7 +49,38 @@ db.collection("Bathroom").doc("bcit.0002").onSnapshot(function (snap) {
         .Like; //Get like
     console.log("Current likes: ", snap.data().Like);
 
-    setAddListener("bcit.0002");
+    setAddListener("3");
+});
+
+db.collection("Bathroom").doc("b4").onSnapshot(function (snap) {
+    console.log("Current data is ... ", snap.data().name);
+    document.getElementById("name4").innerHTML = snap.data().name; //Get name value from document
+    document.getElementById("rating4").innerHTML = snap.data()
+        .rating; //Get rating value from document
+    document.getElementById("location4").innerHTML = snap.data()
+        .location; //Get location value from document
+
+    document.getElementById("likes4").innerHTML = snap.data()
+        .Like; //Get like
+    console.log("Current likes: ", snap.data().Like);
+
+    setAddListener("4");
+});
+
+db.collection("Bathroom").doc("b5").onSnapshot(function (snap) {
+    console.log("Current data is ... ", snap.data().name);
+    document.getElementById("name5").innerHTML = snap.data()
+        .name; //Get name value from document
+    document.getElementById("rating5").innerHTML = snap.data()
+        .rating; //Get rating value from document
+    document.getElementById("location5").innerHTML = snap.data()
+        .location; //Get location value from document
+
+    document.getElementById("likes5").innerHTML = snap.data()
+        .Like; //Get like
+    console.log("Current likes: ", snap.data().Like);
+
+    setAddListener("5");
 });
 
 
@@ -87,17 +89,17 @@ function showLikes(n) {
         db.collection("Bathroom")
         .doc("b"+n)
             .onSnapshot(function (d) {
-                console.log("Current data: ", d.data().likes);
-                document.getElementById(("likes"+n).innerHTML = d.data().likes);
+                console.log("Current data: ", d.data().Like);
+                document.getElementById(("likes"+n).innerHTML = d.data().Like);
             });
     })
 }
 
 
 function setAddListener(n) {
-    document.getElementById("bathroom"+n).addEventListener("click", function (e) {
+    document.getElementById("bathroom" + n).addEventListener("click", function (e) {
 
-        var like = db.collection("Bathroom").doc("b"+n);
+        var like = db.collection("Bathroom").doc("b" + n);
 
         var increment = firebase.firestore.FieldValue.increment(1);
         console.log("showing" + increment);
@@ -108,7 +110,7 @@ function setAddListener(n) {
                 Like: increment
             })
             .then(function () {
-                showLikes();
+                showLikes(n);
             })
             .catch(function (error) {
                 console.error("Error writing document: ", error);
