836375911df1e5617d4440e04fae00a443da7284
Andy Takahashi <a.takahashi13@gmail.com>
2019-12-05 14:20:11 -0800

    fixed some stuff

--- scripts/add.js
+++ scripts/add.js
@@ -1,5 +1,6 @@
+var i = 0;
 $(document).ready(function () {
-    var i = 0;
+
     // Your web app's Firebase configuration
     var firebaseConfig = {
         apiKey: "AIzaSyD_pDCpAP6mtBObEgjgFJWQ7uDmeAZt8ng",
@@ -10,13 +11,13 @@ $(document).ready(function () {
         messagingSenderId: "832183689756",
         appId: "1:832183689756:web:6a44c4e8616fe77e0ef5d9"
     };
-    if(!firebase.apps.length) {
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    if (!firebase.apps.length) {
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
     }
     var db = firebase.firestore();
 
-    $("#addBathRoom").submit(function (e) {
+    $(".add-box").submit(function (e) {
         e.preventDefault();
         console.log("Hello");
         var location = $("#location").val();
@@ -33,13 +34,19 @@ $(document).ready(function () {
             rating: rating
         };
 
-        firebase.auth().onAuthStateChanged(function (user) {
-                i++;
-                db.collection("Bathroom").doc("b" + i)
-                    .add(bathroom);
-            })
+        firebase.auth().onAuthStateChanged(function (bathroom) {
+            
+            db.collection("Bathroom").doc("b" + i).set({
+                Like: 1,
+                name: name,
+                location: location,
+                rating: rating
+            },{merge: true,})
+            $(".add-box").toggle("clip");
+            i++;
         })
-    });
+        
 
 
+    })
 });
